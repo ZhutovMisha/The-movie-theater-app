97c94fbbea8e30dcf493701d046da6f30e38d9ec
ZhutovMisha <107176820+ZhutovMisha@users.noreply.github.com>
2022-06-21 18:28:11 +0300

    Created a project

---
+++ Movie Theater App/Movie Theater App/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Theater App
+//
+//  Created by Brad on 6/21/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
